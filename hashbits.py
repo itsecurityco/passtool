61ae637664c79cb399e9c28afb3656e59627f74e
Juan <juanmauricioescobar@gmail.com>
2020-04-24 07:42:51 -0400

    Create hashbits.py

---
+++ hashbits.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Author: Juan (@itsecurityco)
+# Usage: ./hashbits.py 1d6ce93c79c8c3aca3312268e8ee30bccf8b534e498d639307c276d502bdbf356f8b309bfc1817225d7f6a11460b15ca
+# 384.0
+
+import sys
+print len(sys.argv[1])/2*8
